f7e0b157a054a17bfe981619f9912a679258e898
Fernando Val <aaromnido@gmail.com>
2016-10-04 21:23:44 +0200

    Translate plugin and ES language installed

--- storage/cms/cache/10/59/home.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms57f3ee9e3feac096365294_2568911116Class extends \Cms\Classes\PageCode
-{
-
-}
--- storage/cms/cache/27/59/home.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms57f3ed2c88c3b993411997_575451784Class extends \Cms\Classes\PageCode
-{
-
-}
